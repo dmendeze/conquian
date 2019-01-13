f54fe05ba46530becc3d22070e132b72848e5426
dmendeze <danielestrada36@gmail.com>
2019-01-13 18:49:19 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
+/* eslint max-len: 0 */
+/* eslint no-alert: 0 */
+/* eslint guard-for-in: 0 */
+/* eslint no-unused-vars: 0 */
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+export default class App extends Component {
 
-export default App;
+    constructor () {
+        super();
+        this.state = {
+            cardsInHand: [],
+            cardsInAI: [],
+            deck: [],
+            cardsDiscarded: [],
+            turn: true,
+        }
+
+        this.generateCards = this.generateCards.bind(this);
+
+    }
+
+    componentDidMount() {
+        this.generateCards();
+    }
+
+    generateCards() {
+
+        const suits = ['clubs', 'coins', 'cups', 'swords'];
+        const deck = [];
+
+        for (let i = 1; i < 13; i++) {
+            for (let j = 0; j < 4; j++) {
+                const newCard = {
+                    suit: suits[j],
+                    value: i,
+                    face: suits[j]+i.toString()
+                }
+                deck.push(newCard);
+            }
+        }
+
+        this.setState({deck: deck});
+
+    };
+
+    render ()  {
+        return (
+            <div>
+                {this.state.deck.map((card, index) => (
+                    <p key={index}>{card.value} of {card.suit}</p>
+                ))}
+            </div>
+        );
+    }
+
+};
